c8f632d821d26b560e2dc41a99bc3851417db397
Khurram Shehzad <khurram.pak522@gmail.com>
2019-11-21 13:25:41 +0500

    Initial project

---
+++ EMailVerifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMailVerifier
+//
+//  Created by Khurram Shehzad on 21/11/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
